abe0cb18ce9925c02f1ba761f40bbec888374788
SaketR3 <saketr3@illinois.edu>
2024-09-24 21:30:04 -0500

    Create editing and creating notes endpoints

--- ShareAThought/Controllers/NotesController.cs
+++ ShareAThought/Controllers/NotesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShareAThought.Controllers
 {
@@ -27,5 +28,55 @@ namespace ShareAThought.Controllers
 
         // update note
         // POST: api/Notes/5
+
+        [HttpPost, ActionName("Edit")]
+        public async Task<ActionResult> EditNote(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noteToUpdate = await _context.Notes.FirstOrDefaultAsync(s => s.NoteId == id);
+
+            // _context.Authors.Add(author);
+            // await _context.SaveChangesAsync();
+
+            // return CreatedAtAction("GetAuthor", new { id = author.AuthorId }, author);
+
+            if (noteToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            if (await TryUpdateModelAsync<Note>(
+                noteToUpdate,
+                "",
+                s => s.AuthorId, s => s.Title, s => s.Body, s => s.Author))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+            }
+            return Ok(noteToUpdate);
+        }
+
+        [HttpPost("create"), ActionName("Create")]
+        public async Task<ActionResult<Note>> PostNote(Note note)
+        {
+            _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNote", new { id = note.NoteId }, note);
+        }
     }
 }
